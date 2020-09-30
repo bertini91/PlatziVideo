800492a5a39cac91efb4cb4c26a8c3f794525fb2
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2020-09-30 14:06:20 -0300

    estructura base, terminada

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//render(componente, dondeEscribir)
+ReactDom.render(component, document.getElementById('app'))
+
+
+export default index;
